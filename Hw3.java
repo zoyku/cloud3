b05d668bce52b6d2754fdbee50bff81bc23b39df
ZEYNEP OYKU BUYUKBAY <e2521318@ceng.metu.edu.tr>
2024-06-03 11:44:45 +0000

    finito

--- Hw3.java
+++ Hw3.java
@@ -1,7 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -9,6 +8,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Partitioner;
 
 import java.io.*;
 
@@ -17,11 +17,11 @@ public class Hw3 {
     public static class TotalMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private final static Text word = new Text("Total Salary:");
         private DoubleWritable salary = new DoubleWritable();
-        private boolean isHeader = true;
+        private boolean label = true;
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            if (isHeader) {
-                isHeader = false;
+            if (label) {
+                label = false;
                 return;
             }
 
@@ -48,11 +48,11 @@ public class Hw3 {
     public static class JobTitleMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private Text jobTitle = new Text();
         private DoubleWritable salary = new DoubleWritable();
-        private boolean isHeader = true;
+        private boolean label = true;
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            if (isHeader) {
-                isHeader = false;
+            if (label) {
+                label = false;
                 return;
             }
 
@@ -67,32 +67,35 @@ public class Hw3 {
     public static class TitleExperienceMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private Text jobExp = new Text();
         private DoubleWritable salary = new DoubleWritable();
-        private boolean isHeader = true;
+        private boolean label = true;
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            if (isHeader) {
-                isHeader = false;
+            if (label) {
+                label = false;
                 return;
             }
 
             String[] fields = value.toString().split(",");
             String jobTitle = fields[3];
             String experience = fields[1];
-            jobExp.set(jobTitle + "_" + experience);
-            double sal = Double.parseDouble(fields[6]);
-            salary.set(sal);
-            context.write(jobExp, salary);
+
+            if (experience.equalsIgnoreCase("SE") || experience.equalsIgnoreCase("MI") || experience.equalsIgnoreCase("EX")) {
+                jobExp.set(jobTitle + ":" + experience);
+                double sal = Double.parseDouble(fields[6]);
+                salary.set(sal);
+                context.write(jobExp, salary);
+            }
         }
     }
 
     public static class EmployeeResidenceMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private Text residence = new Text();
         private DoubleWritable salary = new DoubleWritable();
-        private boolean isHeader = true;
+        private boolean label = true;
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            if (isHeader) {
-                isHeader = false;
+            if (label) {
+                label = false;
                 return;
             }
 
@@ -113,11 +116,11 @@ public class Hw3 {
     public static class AverageYearMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         private Text yearPartition = new Text();
         private DoubleWritable salary = new DoubleWritable();
-        private boolean isHeader = true;
+        private boolean label = true;
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            if (isHeader) {
-                isHeader = false;
+            if (label) {
+                label = false;
                 return;
             }
             
@@ -152,9 +155,31 @@ public class Hw3 {
         }
     }
 
+    public static class EmployeeResidencePartitioner extends Partitioner<Text, DoubleWritable> {
+        public int getPartition(Text key, DoubleWritable value, int numReduceTasks) {
+            if (key.toString().equals("US")) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    public static class AverageYearPartitioner extends Partitioner<Text, DoubleWritable> {
+        public int getPartition(Text key, DoubleWritable value, int numReduceTasks) {
+            if (key.toString().equals("2024")) {
+                return 0;
+            } else if (key.toString().equals("2023")) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Hw3");
+        Job job = Job.getInstance(conf, args[0]);
         job.setJarByClass(Hw3.class);
         if (args[0].equals("total")) {
             job.setMapperClass(TotalMapper.class);
@@ -172,17 +197,21 @@ public class Hw3 {
             job.setMapperClass(TitleExperienceMapper.class);
             job.setReducerClass(AvgSalaryReducer.class);
             job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(IntWritable.class);
+            job.setOutputValueClass(DoubleWritable.class);
         }
         else if (args[0].equals("employeeresidence")) {
             job.setMapperClass(EmployeeResidenceMapper.class);
             job.setReducerClass(AvgSalaryReducer.class);
+            job.setPartitionerClass(EmployeeResidencePartitioner.class);
+            job.setNumReduceTasks(2);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(DoubleWritable.class);
         }
         else if (args[0].equals("averageyear")) {
             job.setMapperClass(AverageYearMapper.class);
             job.setReducerClass(AvgSalaryReducer.class);
+            job.setPartitionerClass(AverageYearPartitioner.class);
+            job.setNumReduceTasks(3);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(DoubleWritable.class);
         }
